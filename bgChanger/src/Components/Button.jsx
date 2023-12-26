a7e50654150836dfa62625100ce2e06435bd18b8
MoinZargar <moin_85btech20@nitsri.ac.in>
2023-12-26 21:21:45 +0530

    custom hook

--- bgChanger/src/Components/Button.jsx
+++ bgChanger/src/Components/Button.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 function Button({color="green"}){
-    // const buttonClass = `text-white bg-${color}-700 focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2`;
     const changeBg=()=>{
        document.body.style.backgroundColor = color; 
     }
     
     return(
-        <button type="button" onClick={changeBg} className='text-white bg-blue-700 focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2'>{color}</button>
+        <button type="button" onClick={changeBg} style={{backgroundColor:color}} className='text-white focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2'>{color}</button>
 
     )
    
